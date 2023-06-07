741eb1adc998c40b4d92521015b4db7ae5916d2a
evgeshkins <kidixcomp@mail.ru>
2023-06-08 01:23:03 +0500

    lab5_bd_upd

--- src/logic.php
+++ src/logic.php
@@ -24,12 +24,3 @@ else
     header('Content-Type: application/json');
     echo json_encode("ошибка");
 }
-// Открываем JSON файл
-// $jsonFile = '../data/objects.json';
-// $data = file_get_contents($jsonFile);
-// Декодируем JSON данные в ассоциативный массив
-// $jsonData = json_decode($data, true);
-
-// Отправляем JSON данные на фронтенд
-// header('Content-Type: application/json');
-// echo json_encode($jsonData);
